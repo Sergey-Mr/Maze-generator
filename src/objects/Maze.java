247f08fad6ecbd8b6e8c501502d18b07ffa61243
Serhii <sergey.st265@gmail.com>
2024-06-27 15:47:28 +0100

    Javadoc for Maze.java

--- src/objects/Maze.java
+++ src/objects/Maze.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Represents a maze with a grid of cells in a 2D array.
+ * Each cell can be visited or unvisited and has four walls.
+ */
 public class Maze {
     public int rows;
     public int columns;
     public Cell[][] maze;
 
+    /**
+     * Constructs a new Maze with the given number of rows and columns.
+     * @param rowsIn the number of rows in the maze
+     * @param colsIn the number of columns in the maze
+     */
     public Maze(int rowsIn, int colsIn){
         this.rows = rowsIn;
         this.columns = colsIn;
@@ -16,6 +25,9 @@ public class Maze {
         generateEmptyMaze();
     }
 
+    /**
+     * Generates an empty maze with all cells unvisited and all walls intact (set to true).
+     */
     public void generateEmptyMaze(){
         this.maze = new Cell[this.rows][this.columns];
 
@@ -26,9 +38,14 @@ public class Maze {
         }
     }
 
+    /**
+     * Gets one of unvisited neighbours of the cell at the given row and column.
+     * @param row the row of the cell
+     * @param column the column of the cell
+     * @return an unvisited neighbour of the cell, or null if there are no unvisited neighbours
+     */
     public Cell getUnvisitednNeighbour(int row, int column){
-        // Selects the next cell to go to
-
+        // Contains all unvisited neighbours of the current cell
         List<Cell> unvisitedNeighbours = new ArrayList<>();
 
         // Check the cell above
@@ -61,9 +78,12 @@ public class Maze {
         return null;
     }
 
+    /**
+     * Removes the wall between the current cell and the next cell.
+     * @param current the current cell
+     * @param next the next cell
+     */
     public void removeWall(Cell current, Cell next){
-        // Removes the walls of the cells in the grid
-
         // Get the relevant position of the next cell
         int dx = next.getColumn() - current.getColumn();
         int dy = next.getRow() - current.getRow();
@@ -89,32 +109,60 @@ public class Maze {
 
     }
 
+    /**
+     * Gets the cell from the maze array at the given row and column.
+     * @param rowIn the row of the cell
+     * @param columnIn the column of the cell
+     * @return the cell at the given row and column
+     * @throws IndexOutOfBoundsException if the row or column is out of bounds
+     */
     public Cell getCellByIndex(int rowIn, int columnIn){
         if (rowIn >= 0 && rowIn < this.getRows() && columnIn >= 0 && columnIn < this.getColumns()) {
+            // If coordinates exist in the maze, return the cell
             return this.getMaze()[rowIn][columnIn];
         }
-
         else {
+            // Throw exception otherwise
             throw new IndexOutOfBoundsException("Cell index out of boundes");
         }
     }
 
+    /**
+    * Gets the number of rows in the maze.
+    * @return the number of rows in the maze
+    */
     public int getRows(){
-        return rows;
+        return this.rows;
     }
 
+    /**
+    * Gets the number of columns in the maze.
+    * @return the number of columns in the maze
+    */
     public int getColumns(){
-        return columns;
+        return this.columns;
     }
 
+    /**
+    * Sets the number of columns in the maze.
+    * @param colsIn the new number of columns in the maze
+    */
     public void setColumns(int colsIn){
         this.columns = colsIn;
     }
 
+    /**
+    * Sets the number of rows in the maze.
+    * @param rowsIn the new number of rows in the maze
+    */
     public void setRows(int rowsIn){
         this.rows = rowsIn;
     }
 
+    /**
+    * Gets the 2D array of cells representing the maze.
+    * @return the 2D array of cells representing the maze
+    */
     public Cell[][] getMaze(){
         return this.maze;
     }
